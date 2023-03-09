150d7a8c8475f5497bef8a9ad1461db3ae7247ca
Sergei <sandeeey@example.com>
2023-03-10 01:55:35 +0300

    task_56

--- task_56/Program.cs
+++ task_56/Program.cs
@@ -19,6 +19,8 @@ Console.WriteLine($"\n Заданый массив: \n");
 int[,] resultMatrix = GetMatrix(6, 4, 0, 10);
 PrintMatrix(resultMatrix);
 
+SumMatrix(resultMatrix);
+
 
 /// <summary>
 /// Заполнение двумерного массива рандомными элементами от min до max.
@@ -58,12 +60,33 @@ void PrintMatrix(int[,] matr)
 
 }
 
+/// <summary>
+/// Метод находит номер строки с наименьшим элементом
+/// </summary>
+/// <param name="matr">Двумерный массив</param>
 void SumMatrix(int[,] matr)
 {
+
+    int index = 0;
+    int minSum = int.MaxValue;
+
     for (int i = 0; i < matr.GetLength(0); i++)
     {
-         int sum=0;
-         int sum2=0;
-         int result=0;
+        int sum = 0;
 
         for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            sum += matr[i, j];
+
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            index = i;
+
+        }
+
+    }
+
+    Console.WriteLine($"\n Номер строки с наименьшей суммой элементов: {index + 1} \n");
+}
